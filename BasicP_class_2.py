a180a77b81672f94e8e99cb9f233decf2cb2468d
lavendef <152055839+lavendef@users.noreply.github.com>
2025-07-22 16:15:30 +0700

    add BasicP_class_2 and workshop

---
+++ BasicP_class_2.py
@@ -0,0 +1,63 @@
+# x = "WTF"
+# print("ค่าในตัวแปร x:",x)
+# y = 20.99
+# print('ค่าในตัวแปร y:',y)
+# print("type ของ x",type(x))
+# b = False
+# print("type ของ b",type(b))
+# print("type ของ y",type(y))
+# //////////////////////////////////////////////////////////////////////
+# nom = "123"
+# print("ค่าของ nom x:",nom)
+# print("type ของ nom",type(nom))
+# print("หลังจากแปลงค่า")
+# print("type ของ nom",type(int(nom)))
+# //////////////////////////////////////////////////////////////////////
+# + - * / % **
+# mod = 10 % 4
+# print(mod)
+# expo = 99**99
+# print(expo)
+# ten = 10
+# one = 1
+# print (ten == one)
+# print (ten != one)
+# print (ten > one)
+# T = True
+# F = False
+# print(T and F)
+# print(T or F)
+# num = int(input("กรอกเลขได้ตรงนี้:"))
+# print("num =",num)
+# print(type(num))
+
+# introd = input()
+# print(introd)
+# print(type(introd))
+
+# x=9
+# y=7
+
+# if x==y:
+#     print('ใน if:',"x == y")
+# elif x>y:
+#     print('ใน else if:', "x > y")
+# else:
+#     print("ใน else:","x != y")
+# print("นอก if:","What is this")
+
+# somebool = input()
+# if type(somebool) == int:
+#     somebool = False
+# elif type(somebool) == str:
+#     somebool = True
+#     if somebool == True:
+#         print("let's goooo")
+#     elif somebool == False:
+#         print(":(")
+#     else:
+#         print("plase try agian")
+
+# else:
+#     print("i'm crying")
+
---
+++ BasicP_class_2_workshop.py
@@ -0,0 +1,11 @@
+rayatann = int(input("คุณพี่จะส่งเท่าไรค่ะ :"))
+if 5 <= rayatann <= 50:
+    print("ราคาค่าส่ง = 10 บาทค่ะ")
+elif 51 <= rayatann <= 100:
+    print("ราคาค่าส่ง = 15 บาทค่ะ")
+elif 101 <= rayatann <= 300:
+    print("ราคาค่าส่ง = 25 บาทค่ะ")
+elif 301 <= rayatann <= 500:
+    print("ราคาค่าส่ง = 35 บาทค่ะ")
+else:
+    print("ราคาค่าส่ง = 45 บาทค่ะ")
